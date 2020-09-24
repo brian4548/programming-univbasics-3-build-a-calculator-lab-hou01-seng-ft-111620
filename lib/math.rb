9c38edcae31a9d0ea52a117bee68b816af87e2af
Brian Rhodes <brianr4548@gmail.com>
2020-09-24 01:30:21 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -6,13 +6,13 @@ addition(5,4)
 
 
 def subtraction(num1, num2)
-num1 - num2
+  num1 - num2
 end
 
 subtraction(10,5)
 
 def division(num1, num2)
-num1 / num2
+  num1 / num2
 end
 
 division(50,2)
@@ -21,9 +21,10 @@ def multiplication(num1, num2)
 num1 * num2
 end
 
-multiplication()
+multiplication(4,30)
 
 def modulo(num1, num2)
+
 
 end
 
